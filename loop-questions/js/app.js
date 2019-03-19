0f1d01ff2da91007e96e825c11988fac93c11b07
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-19 16:37:56 -0700

    Commit 9 - Created loops.js file

---
+++ loop-questions/js/app.js
@@ -0,0 +1,37 @@
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+
+for (let i = 99; i >= 0; i--) {
+  console.log(i);
+}
+
+for (let i = 298; i > 0 ; i-=3) {
+  console.log(i);
+}
+
+for (let i = 0; i < 99; i++) {
+  if (i %2 === 0){
+    console.log(i);
+  }
+}
+
+for (let i = 98; i >=0 ; i--) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+for (let i = 49; i < 73; i++) {
+  console.log(i);
+}
+
+for (let i = 81; i > 25 ; i--) {
+  console.log(i);
+}
+
+for (let i = 1; i < 92; i++) {
+  if (i % 3 === 0) {
+    console.log(i);
+  }
+}
